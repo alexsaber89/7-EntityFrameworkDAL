1faceeb1222a0e1e235db425f41301f9ee92bfce
Alex Saber <alexsaber89@gmail.com>
2017-03-06 17:27:21 -0600

    implement Song entity class

---
+++ MusicHistoryCS/Models/Song.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MusicHistoryCS.Models
+{
+    public class Song
+    {
+        [Key]
+        public int SongID { get; set; }
+
+        public string Name { get; set; }
+
+        public int Duration { get; set; }
+
+        public int MyProperty { get; set; }
+    }
+}
