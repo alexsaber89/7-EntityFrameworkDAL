0f9859dcbc0c70d84c6276ee4449503983602f2f
Alex Saber <alexsaber89@gmail.com>
2017-03-06 17:25:59 -0600

    implement Album entity class

---
+++ MusicHistoryCS/Models/Album.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MusicHistoryCS.Models
+{
+    public class Album
+    {
+        [Key]
+        public int AlbumID { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        public ICollection<Artist> Artists { get; set; }
+    }
+}
