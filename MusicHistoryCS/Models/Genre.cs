0952e2bf49c52fea1af18f52633f9bfd40e78d2d
Alex Saber <alexsaber89@gmail.com>
2017-03-06 17:26:59 -0600

    implement Genre entity class

---
+++ MusicHistoryCS/Models/Genre.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MusicHistoryCS.Models
+{
+    public class Genre
+    {
+        [Key]
+        public int GenreID { get; set; }
+
+        public string Name { get; set; }
+
+        public ICollection<Artist> Artists { get; set; }
+    }
+}
