a11654480f95ef6ad6393675e76c431a50c7f6ed
Alex Saber <alexsaber89@gmail.com>
2017-03-06 17:26:42 -0600

    implement Artist entity class

---
+++ MusicHistoryCS/Models/Artist.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MusicHistoryCS.Models
+{
+    public class Artist
+    {
+        [Key]
+        public int ArtistID { get; set; }
+
+        public string Name { get; set; }
+
+        public int MyProperty { get; set; }
+
+        public ICollection<Artist> Artists { get; set; }
+    }
+}
